eccbd22d5bf87369ac1fdd1119fa26de930ca450
Ben Brinckerhoff <ben@bbrinck.com>
2016-12-07 18:33:01 -0700

    Adds text fields for all sets

--- src/hundred_pushups/ios/core.cljs
+++ src/hundred_pushups/ios/core.cljs
@@ -13,6 +13,7 @@
 (def text-input (r/adapt-react-class (.-TextInput ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
+(def scroll-view (r/adapt-react-class ReactNative.ScrollView))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (def pushup-form-url "http://www.100pushups.com/perfect-pushups-posture/")
@@ -50,6 +51,7 @@
        [text {} "Reps:"]
        [text-input {:style {:height 40 :border-color "grey" :border-width 1}
                     :default-value (:pushup-reps-text @ui-state)
+                    :keyboard-type "numeric"
                     :on-change-text (fn [text]
                                  (dispatch [:ui-state/set [:pushup-reps-text] text])
                                       (dispatch [:db/save]))}]
@@ -68,9 +70,10 @@
        [text {:style {:text-align "center"}} "Stop when your form starts to get bad (e.g. sagging body)"]
        [text {} "Plank breadths:"]
        [text-input {:style {:height 40 :border-color "grey" :border-width 1}
+                    :keyboard-type "numeric"
                     :default-value (:plank-reps-text @ui-state)
                     :on-change-text (fn [text]
-                                 (dispatch [:ui-state/set [:plank-reps-text] text])
+                                      (dispatch [:ui-state/set [:plank-reps-text] text])
                                       (dispatch [:db/save]))}]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5 :margin 10}
                              :on-press #(do
@@ -98,17 +101,31 @@
    [text {:style {:font-size 20 :font-weight "100" :margin-bottom 10 :text-align "center"}} "Today's exercise"]
    (let [schedule @(subscribe [:days-exercise])
          circuit (:exr/circuit schedule)]
-     [view {}
-      (for [x (range (:exr/sets schedule))]
-        [view {:key x}
-         [text {:style {:font-size 18 :font-weight "600" :margin-top 10}} (str "Set" x)]
-         [text {} (str (:exr/pushup-reps circuit) " pushups")]
-         [text {} (str "Hold plank for "(:exr/plank-reps circuit) " breaths")]])
+     [view {:style {:height 350 :width 250 :padding 20 :background-color "lightgrey"}}
+      [scroll-view {:style {:flex 1}}
+       (for [x (range (:exr/sets schedule))]
+         [view {:key x}
+          [text {:style {:font-size 18 :font-weight "600" :margin-top 10}} (str "Set" x)]
+          [text {} (str (:exr/pushup-reps circuit) " pushups")]
+          [text-input {:style {:height 40 :border-color "grey" :border-width 1}
+                       :default-value (str (:exr/pushup-reps circuit))
+                       :keyboard-type "numeric"
+                       :on-change-text (fn [text]
+                                         (dispatch [:ui-state/set [:pushup-reps-text x] text])
+                                         (dispatch [:db/save]))}]
+          [text {} (str "Hold plank for "(:exr/plank-reps circuit) " breaths")]
+          [text-input {:style {:height 40 :border-color "grey" :border-width 1}
+                       :default-value (str (:exr/plank-reps circuit))
+                       :keyboard-type "numeric"
+                       :on-change-text (fn [text]
+                                         (dispatch [:ui-state/set [:plank-reps-text x] text])
+                                         (dispatch [:db/save]))}]])]
       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5 :margin-top 20}
                             :on-press #(do
                                          (dispatch [:complete-day schedule])
                                          (dispatch [:db/save]))}
-       [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "I did it"]]])])
+       [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Done!"]]]
+     )])
 
 (defn app-root []
   (let [stage (subscribe [:stage])]
