304c5f66d70fdc1997e184f5c15f9c09600db10f
Ben Brinckerhoff <ben@bbrinck.com>
2017-01-09 19:56:24 -0700

    Deletes a little more 'mode'-related code

--- src/hundred_pushups/android/core.cljs
+++ src/hundred_pushups/android/core.cljs
@@ -156,12 +156,6 @@
                                               (dispatch [:db/save]))
                                              ))}
          [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Save Schedule"]]
-        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5 :margin-top 20}
-                              :on-press #(do
-                                           (dispatch [:ui-mode-set [:current-mode] :stages])
-                                           (dispatch [:db/save]))}
-         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Back"]]
-
 
         [picker {:selected-value (:schedule-day-text @ui-state)
                  :on-value-change (fn [item-data]
@@ -206,10 +200,6 @@
                                               (dispatch [:db/save]))}
             [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Remove"]]])]]])))
 
-
-(defn invalid-mode []
-  [view {:style {:flex-direction "column" :align-items "center"}}
-   [text {:style {:font-size 20 :font-weight "100" :margin-bottom 10 :text-align "center"}} "Bad user, how did you get here."]])
 
 (defn show-stage [stage]
   [view {:style {:flex-direction "column" :align-items "center"}}
